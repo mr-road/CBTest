8b83c923cf2dc00d7495def0a83774ab783532f5
James Atherton <james.atherton@fireflylearning.com>
2020-11-27 15:52:25 +0000

    Adding initial test to setup framework, and check everything works.

---
+++ ClearBank.DeveloperTest.Tests/InitialTest.cs
@@ -0,0 +1,17 @@
+﻿using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    public class InitialTest
+    {
+        [Fact]
+        public void GivenEmptyPaymentRequest_WhenMakingPayment_ThenNoExceptionsThrownAndSuccessEqualsFalse()
+        {
+            var paymentService = new PaymentService();
+            var result = paymentService.MakePayment(new MakePaymentRequest());
+            Assert.False(result.Success);
+        }
+    }
+}
